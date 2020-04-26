7214a132b84c081ac4ffcedd3d777702df5ecc9f
LucasFacul31 <61568542+LucasFacul31@users.noreply.github.com>
2020-04-26 18:05:12 -0300

    Add files via upload

---
+++ portal_realnews_dump.sql
@@ -0,0 +1,39 @@
+﻿INSERT INTO noticia (
+  titulo,
+  descricao,
+  texto
+) VALUES (
+  'Notícia 1',
+  'Descrição da notícia 1',
+  'Texto teste para a notícia 1.'
+);
+
+INSERT INTO noticia (
+  titulo,
+  descricao,
+  texto
+) VALUES (
+  'Notícia 2',
+  'Descrição da notícia 2',
+  'Texto teste para a notícia 2.'
+);
+
+INSERT INTO noticia (
+  titulo,
+  descricao,
+  texto
+) VALUES (
+  'Notícia 3',
+  'Descrição da notícia 3',
+  'Texto teste para a notícia 3.'
+);
+
+INSERT INTO comentario (
+  nome,
+  texto,
+  fk_noticia_id
+) VALUES (
+  'Lucas',
+  'Comentario teste',
+  3
+)
