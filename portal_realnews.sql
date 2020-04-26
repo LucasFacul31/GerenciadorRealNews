9e9ac044884e5bbe52cc4da2a6a5f928948f99b9
lucaslopes31 <56418336+lucaslopes31@users.noreply.github.com>
2020-04-26 14:34:27 -0300

    BD and connector

---
+++ portal_realnews.sql
@@ -0,0 +1,16 @@
+﻿DROP TABLE IF EXISTS portal_realnews;
+CREATE DATABASE portal_realnews; 
+USE portal_realnews; 
+CREATE TABLE noticia ( 
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(512),
+  titulo VARCHAR(126),
+  texto TEXT
+);
+CREATE TABLE comentario ( 
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(126),
+  texto VARCHAR(512),
+  fk_noticia_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (fk_noticia_id) REFERENCES noticia(id)
+);
